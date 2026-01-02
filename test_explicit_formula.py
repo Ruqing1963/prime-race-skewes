62e88295787a87a983dbce441ce0b7e7cec2d3b2
Ruqing1963 <ruqing@hotmail.com>
2026-01-02 16:37:47 -0500

    Add files via upload

---
+++ test_explicit_formula.py
@@ -0,0 +1,12 @@
+"""Tests for explicit_formula module"""
+import unittest
+import numpy as np
+from src.explicit_formula import *
+
+class TestExplicit_Formula(unittest.TestCase):
+    def test_basic(self):
+        """Basic functionality test"""
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
